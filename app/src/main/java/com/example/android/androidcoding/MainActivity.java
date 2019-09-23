75f780742027d59bfdb5811cd459ca34092c6da2
Dwayne Thomas <dthom174@fiu.edu>
2019-09-23 02:55:51 -0400

    Created a user profile class and adapter to create and hold the list

--- app/src/main/java/com/example/android/androidcoding/MainActivity.java
+++ app/src/main/java/com/example/android/androidcoding/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.android.androidcoding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private UserProfileAdapter mAdapter;
+    private static final int PROFILE_LOADER_ID = 1;
+    private static final String USG_URL = "https://reqres.in/api/users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Find a reference to the {@link ListView} in the layout
+        ListView listOfUser = (ListView) findViewById(R.id.profile_list);
+
+        ArrayList<UserProfile> dummy = new ArrayList<>();
+        dummy.add(new UserProfile("Dwayne", "Thomas", ""));
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        mAdapter = new UserProfileAdapter(this, new ArrayList<UserProfile>());
+        listOfUser.setAdapter(mAdapter);
     }
 }
